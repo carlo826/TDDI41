de12914c8fe5af516401fbd061bd8f5b2979be2b
Carl Lorentsson <carlo826@student.liu.se>
2020-01-11 18:44:37 +0100

    Update nfs_test3.sh

--- tests/nfs_test3.sh
+++ tests/nfs_test3.sh
@@ -13,7 +13,7 @@ if [ $HOSTNAME == "server" ]; then
 ###     Check if the the automounter is working properly
 else
     ## Check that home1 is not already mounted
-    [[ $(ls /home | ! grep 'home1') ]] &&
+    [[! $(ls /home | grep 'home1') ]] &&
     ## Check that home1 is mounted when entered into and that it contains user1
     [[ $(cd /home/home1 && ls | grep 'user1') ]] &&
     echo "Test successful" && exit 0
