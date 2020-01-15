ad6bd6b33455f98b5eb91c81a9decab4ce2b14a7
Frans Bergström <frabe808@student.liu.se>
2020-01-15 13:54:49 +0100

    fixed syntax net_test.sh

--- tests/net_test.sh
+++ tests/net_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 arr=("gw" "server" "client-1" "client-2")
-echo "Test gw(1), server(2), client-1(3), client-2(4)?
-read -n 1 -p "Selection:" input
+echo "Test gw(1), server(2), client-1(3), client-2(4)?"
+read  -n 1 -p "Selection:" input
 
 if [ $HOSTNAME == arr[input] ]; then
     echo "Test successful" && exit 0
